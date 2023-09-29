2462e510349338a5dc8f815f0e82e99da3ebd712
Audyari <audy123ari@gmail.com>
2023-09-29 08:27:31 +0700

    Class Java String Basic

--- JavaStringClass.java
+++ JavaStringClass.java
@@ -1,8 +1,25 @@
+import java.util.Locale;
+
 public class JavaStringClass {
 
     public static void main(String[] args) {
 
-        System.out.println("test");
+        String nama = "AUDYARI WIYONO";
+
+        String HurufKecil = nama.toLowerCase();
+        int PanjangHuruf = nama.length();
+
+
+        System.out.println(HurufKecil);
+        System.out.println(PanjangHuruf);
+
+        String[] Pisahkan = nama.split(" ");
+        for (String Pisah: Pisahkan){
+            System.out.println(Pisah);
+        }
+
+        char[] KarakterKeArray = nama.toCharArray();
+        System.out.println(KarakterKeArray[0]);
 
     }
 
